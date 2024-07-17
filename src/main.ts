3d8402652b5afcc1f88420ad79b7678fbfc801c0
Guste Gaubaite <219.guste@gmail.com>
2024-07-17 19:19:59 +0200

    Implementado array method filter para extraer lista de pacientes de Pediatría y mostrar la lista en la consola

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,11 @@ import "./style.css";
 
 import { Pacientes, pacientes } from './model';
 
+// Apartado 1: 
+// a) extraer la lista de pacientes que están asignados a la especialidad de Pediatría
+
+const pacientesAsignadosAPediatria : Pacientes[] = pacientes.filter(
+  (paciente: Pacientes) => paciente.especialidad === "Pediatra"
+);
+
+console.log("Lista de pacientes de Pediatria: ", pacientesAsignadosAPediatria);
